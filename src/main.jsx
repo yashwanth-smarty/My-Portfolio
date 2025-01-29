3d8c296de6518f2838ad95aa04a72ea85a38c716
Yashwanth <kumaryashwanth198@gmail.com>
2025-01-29 10:41:27 +0530

    update main and vite

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from './App'; // Your main App component
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* Set basename to match GitHub Pages subdirectory */}
+    <BrowserRouter basename="/My-Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
