752802964414942995808b0fed18566764a2c6f6
Yashwanth <kumaryashwanth198@gmail.com>
2025-01-29 11:18:59 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/My-Portfolio/', // Ensure this matches your repository name
   plugins: [react()],
 });
